7ed3a897e995c48753afa212c90450489c3fd581
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2013-01-16 20:13:20 +0000

    working fine

--- popup.js
+++ popup.js
@@ -1,3 +1,12 @@
 $(function() {
-    $('#qrcode').qrcode('Hello!');
+    var $qrcode = $('#qrcode');
+    
+    $qrcode.html('Wait for it...');
+    
+    chrome.tabs.getSelected(function(the_tab) {
+        if(the_tab && the_tab.url) {
+            $qrcode.html('');
+            $qrcode.qrcode(the_tab.url);
+        }
+    });
 });
