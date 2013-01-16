76064fccd8fb6e781148f8e8e0bfabde3acedd19
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2013-01-16 20:34:02 +0000

    added icons, close button, aligned to the right

--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
 $(function() {
     var $qrcode = $('#qrcode');
+    var $closeme = $('#closeme');
     
     $qrcode.html('Wait for it...');
+    
+    $closeme.click(function() {
+        window.close();
+    });
     
     chrome.tabs.getSelected(function(the_tab) {
         if(the_tab && the_tab.url) {
